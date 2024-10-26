c9e6ee7150c8b56269dd33c3e2e6e2b42b834013
MishkaMedved <mixail.228@bk.ru>
2024-10-26 10:26:38 +0400

    Перенес логику загрузки в сервис

--- backend/src/main/java/ru/mixail/backend/controllers/MainController.java
+++ backend/src/main/java/ru/mixail/backend/controllers/MainController.java
@@ -1,22 +1,13 @@
 package ru.mixail.backend.controllers;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.mixail.backend.models.Report;
 import ru.mixail.backend.services.ReportService;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api")
@@ -35,45 +26,11 @@ public class MainController {
         String fileType = file.getContentType();
 
         if (fileType.equals("text/csv")) {
-            return processCsv(file);
+            return reportService.processCsv(file);
         } else if (fileType.equals("application/json")) {
-            return processJson(file);
+            return reportService.processJson(file);
         } else {
             return ResponseEntity.badRequest().body("Неподдерживаемый формат файла. Пожалуйста, загрузите CSV или JSON.");
-        }
-    }
-
-    private ResponseEntity<?> processCsv(MultipartFile file) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            List<Map<String, String>> records = new ArrayList<>();
-            String line;
-            String[] headers = reader.readLine().split(","); // Заголовки столбцов
-
-            while ((line = reader.readLine()) != null) {
-                String[] values = line.split(",");
-                Map<String, String> record = new HashMap<>();
-                for (int i = 0; i < headers.length; i++) {
-                    record.put(headers[i], values[i]);
-                }
-                records.add(record);
-            }
-
-            // Сохранение records в базу или отправка на фронтенд для отображения(пока что не знаю, что с ними делать)
-            return ResponseEntity.ok(records); // временно возвращаю данные для проверки
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка обработки CSV файла.");
-        }
-    }
-
-    private ResponseEntity<?> processJson(MultipartFile file) {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            List<Map<String, Object>> records = mapper.readValue(file.getInputStream(), new TypeReference<List<Map<String, Object>>>() {});
-
-            // Сохранение records в базу или отправка на фронтенд для отображения(пока что не знаю, что с ними делать)
-            return ResponseEntity.ok(records); // временно возвращаем данные для проверки
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка обработки JSON файла.");
         }
     }
 
--- backend/src/main/java/ru/mixail/backend/services/ReportService.java
+++ backend/src/main/java/ru/mixail/backend/services/ReportService.java
@@ -1,14 +1,21 @@
 package ru.mixail.backend.services;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import ru.mixail.backend.models.Report;
 import ru.mixail.backend.repositories.ReportRepository;
 
-import java.util.List;
-import java.util.Optional;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -26,5 +33,37 @@ public class ReportService {
         return report.get();
     }
 
+    public ResponseEntity<?> processCsv(MultipartFile file) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            List<Map<String, String>> records = new ArrayList<>();
+            String line;
+            String[] headers = reader.readLine().split(","); // Заголовки столбцов
 
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                Map<String, String> record = new HashMap<>();
+                for (int i = 0; i < headers.length; i++) {
+                    record.put(headers[i], values[i]);
+                }
+                records.add(record);
+            }
+
+            // Сохранение records в базу или отправка на фронтенд для отображения(пока что не знаю, что с ними делать)
+            return ResponseEntity.ok(records); // временно возвращаю данные для проверки
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка обработки CSV файла.");
+        }
+    }
+
+    public ResponseEntity<?> processJson(MultipartFile file) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, Object>> records = mapper.readValue(file.getInputStream(), new TypeReference<List<Map<String, Object>>>() {});
+
+            // Сохранение records в базу или отправка на фронтенд для отображения(пока что не знаю, что с ними делать)
+            return ResponseEntity.ok(records); // временно возвращаем данные для проверки
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка обработки JSON файла.");
+        }
+    }
 }
