1c1758df177899648ab00351c290f814878f7474
MishkaMedved <mixail.228@bk.ru>
2024-10-27 16:03:39 +0400

    Исправил Enum и починил запрос на список отчетов

--- backend/src/main/java/ru/mixail/backend/enums/StatusEnum.java
+++ backend/src/main/java/ru/mixail/backend/enums/StatusEnum.java
@@ -1,7 +1,21 @@
 package ru.mixail.backend.enums;
 
 public enum StatusEnum {
-    DRAFT,
-    PUBLISHED,
-    UNDER_CONSIDERATION
+    PUBLISHED("Опубликовано"),
+    DRAFT("Черновик");
+
+    private final String name;
+
+    StatusEnum(String name) {
+        this.name = name;
+    }
+
+    public static StatusEnum fromString(String value) {
+        for (StatusEnum status : StatusEnum.values()) {
+            if (status.name.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant for value: " + value);
+    }
 }
